5d50e88b0d0be482f59f3accc130941387ebd7b6
kkang <kyeongmankang@gmail.com>
2022-06-16 16:35:56 +0900

    lint, pre-commit 규칙을 만족하는 개발 환경 구축 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import styled, {ThemeProvider} from 'styled-components';
-import {theme} from "./styles/theme";
-import {GlobalStyle} from "./styles/global-style";
+import styled, { ThemeProvider } from 'styled-components'
+
+import { theme } from './styles/theme'
+import { GlobalStyle } from './styles/global-style'
+
 const Button = styled.button`
   color: ${theme.color.heavyGray};
   background-color: ${theme.color.white};
@@ -12,20 +14,23 @@ function App() {
 
   return (
     <>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-    <div className="App">
-      <header className="App-header">
-          <Button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </Button>
-      </header>
-    </div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-      </ul>
-    </ThemeProvider>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <Button
+              type="button"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is: {count}
+            </Button>
+          </header>
+        </div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+      </ThemeProvider>
     </>
   )
 }
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,13 +1,13 @@
-import "styled-components";
+import 'styled-components'
 
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme {
     color: {
-      white: "rgba(255, 255, 255, 1)",
-      black: "rgba(0, 0, 0, 0)",
-      heavyGray: "rgba(58, 58, 58)",
-      mediumGray: "rgba(58, 58, 58, 0.8)",
-      lightGray: "rgba(58, 58, 58, 0.7)",
-    };
+      white: 'rgba(255, 255, 255, 1)'
+      black: 'rgba(0, 0, 0, 0)'
+      heavyGray: 'rgba(58, 58, 58)'
+      mediumGray: 'rgba(58, 58, 58, 0.8)'
+      lightGray: 'rgba(58, 58, 58, 0.7)'
+    }
   }
 }
