2dcdfb9ee15339298c816b74814874d6088ac93c
kkang <kyeongmankang@gmail.com>
2022-06-17 17:34:23 +0900

    MetricsContainer 컴포넌트 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { convertPixelToRem } from './utils/helpers'
 import ContentLogo from './components/ContentLogo'
 import SectionContainer from './components/SectionContainer'
 import AwardsContainer from './components/AwardsContainer'
+import MetricsContainer from './components/MetricsContainer'
 
 const ContentsContainer = styled.div`
   width: ${convertPixelToRem(1040)}rem;
@@ -13,33 +14,6 @@ const ContentsContainer = styled.div`
   margin: 0 auto;
   position: relative;
 `
-const MetricsContainer = styled.div`
-  margin-left: ${convertPixelToRem(623)}rem;
-  padding-top: ${convertPixelToRem(150)}rem;
-`
-
-const MetricItemContainer = styled.div`
-  color: ${theme.color.heavyGray};
-  font-family: sans-serif;
-  & strong {
-    font-weight: bold;
-  }
-  font-size: 36px;
-  font-size: ${convertPixelToRem(36)}rem;
-  letter-spacing: -${convertPixelToRem(1)}rem;
-  margin-bottom: ${convertPixelToRem(20)}rem;
-`
-
-function MetricItem() {
-  return (
-    <MetricItemContainer>
-      <strong>
-        <span>100</span>만 개
-      </strong>
-      의 여행 일정
-    </MetricItemContainer>
-  )
-}
 
 function App() {
   return (
@@ -49,11 +23,7 @@ function App() {
         <SectionContainer>
           <ContentsContainer>
             <ContentLogo>2021년 12월 기준</ContentLogo>
-            <MetricsContainer>
-              <MetricItem />
-              <MetricItem />
-              <MetricItem />
-            </MetricsContainer>
+            <MetricsContainer />
             <AwardsContainer />
           </ContentsContainer>
         </SectionContainer>
---
+++ src/components/MetricsContainer.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+import { theme } from '../styles/theme'
+import { convertPixelToRem } from '../utils/helpers'
+
+const StyledContainer = styled.div`
+  margin-left: ${convertPixelToRem(623)}rem;
+  padding-top: ${convertPixelToRem(150)}rem;
+`
+
+const MetricItemContainer = styled.div`
+  color: ${theme.color.heavyGray};
+  font-family: sans-serif;
+  & strong {
+    font-weight: bold;
+  }
+  font-size: 36px;
+  font-size: ${convertPixelToRem(36)}rem;
+  letter-spacing: -${convertPixelToRem(1)}rem;
+  margin-bottom: ${convertPixelToRem(20)}rem;
+`
+
+function MetricItem() {
+  return (
+    <MetricItemContainer>
+      <strong>
+        <span>100</span>만 개
+      </strong>
+      의 여행 일정
+    </MetricItemContainer>
+  )
+}
+
+function MetricsContainer() {
+  return (
+    <StyledContainer>
+      <MetricItem />
+      <MetricItem />
+      <MetricItem />
+    </StyledContainer>
+  )
+}
+
+export default MetricsContainer
